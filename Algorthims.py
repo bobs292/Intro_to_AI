d5320b95a20f3ded650a2972a9e6c71185a42931
bobs292 <jackobrien429@gmail.com>
2026-03-19 13:08:03 +1100

    Update Algorthims.py
    made the indentation neater

--- Algorthims.py
+++ Algorthims.py
@@ -18,13 +18,14 @@ class Greed:
                 print(node)
                 num_node = int(node)
                 cost = self.total_cost_to_goal(graph,num_node)
+                
                 if cost < best_cost:
                     best_cost = cost
                     best_node = node
+                    
             self.current_node = best_node
         self.path.append(self.goal_node)
         return self.path
-
 
     def total_cost_to_goal(self, graph, start):
         queue = [(start,0)]
@@ -40,7 +41,6 @@ class Greed:
 
                 for connect_node in graph.graph[node]:
                     edge_cost = graph.graph[node][connect_node]
-
                     queue.append((connect_node, cost + edge_cost))
                     
         return float('inf')
