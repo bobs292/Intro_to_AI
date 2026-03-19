a3c0dfe2ee1988c6e53923d38c6afe3e97000dd6
bobs292 <jackobrien429@gmail.com>
2026-03-19 16:18:26 +1100

    Update Algorthims.py
    Dumb mistakes left behind and a print during debugging was left behind as well.

--- Algorthims.py
+++ Algorthims.py
@@ -9,25 +9,32 @@ class Greed:
     def search(self,graph):
         while self.current_node != self.goal_node:
             self.path.append(self.current_node)
+            self.visited.add(self.current_node)
             connect_nodes = graph.graph[self.current_node]
 
             best_node = None
             best_cost = float('inf')
             
             for node in connect_nodes:
-                print(node)
+                if node in self.visited:
+                    continue
+                    
                 cost = self.total_cost_to_goal(graph,node)
                 
                 if cost < best_cost:
                     best_cost = cost
                     best_node = node
                     
+            if best_node is None:
+                return None
+                
             self.current_node = best_node
         self.path.append(self.goal_node)
         return self.path
 
     def total_cost_to_goal(self, graph, start):
         queue = [(start,0)]
+        visited = set()
 
         while queue:
             node,cost = queue.pop() 
@@ -35,8 +42,8 @@ class Greed:
             if node == self.goal_node:
                 return cost
             
-            if node not in self.visited:
-                self.visited.add(node)
+            if node not in visited:
+                visited.add(node)
 
                 for connect_node in graph.graph[node]:
                     edge_cost = graph.graph[node][connect_node]
