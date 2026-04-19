e85a91a9ca23482ff986fc6d3e759fdacb200daa
Harry-Webb8 <105364303@student.swin.edu.au>
2026-04-19 15:59:01 +1000

    Add A* search tests to test_graphs.py + 3rd Greed test

--- tests/test_graphs.py
+++ tests/test_graphs.py
@@ -1,4 +1,4 @@
-from algorithms import BFS, Greed, DFS, Beam
+from algorithms import BFS, Greed, DFS, Beam, AS
 from algorithms import parse_input
 
 def test_BFS():
@@ -21,7 +21,7 @@ def test_Greed_1():
 
     assert res == 3
 
-def test_Greed_1():
+def test_Greed_2():
     finput = "PathFinder-test2.txt"
     origin, destinations, graph = parse_input(finput)
 
@@ -31,6 +31,17 @@ def test_Greed_1():
     res = len(a.path)
 
     assert res == 5
+
+def test_Greed_3():
+    finput = "PathFinder-test3.txt"
+    origin, destinations, graph = parse_input(finput)
+
+    a = Greed(origin, destinations, "Greed", finput, graph)
+    a.search()
+
+    res = len(a.path)
+
+    assert res == 3
 
 def test_DFS_1():
     finput = "PathFinder-test.txt"
@@ -75,3 +86,36 @@ def test_Beam_2():
     res = len(a.path)
 
     assert res == 5
+
+def test_AS_1():
+    finput = "PathFinder-test.txt"
+    origin, destinations, graph = parse_input(finput)
+
+    a = AS(origin, destinations, "A*", finput, graph)
+    a.search()
+
+    res = len(a.path)
+
+    assert res == 3
+
+def test_AS_2():
+    finput = "PathFinder-test2.txt"
+    origin, destinations, graph = parse_input(finput)
+
+    a = AS(origin, destinations, "A*", finput, graph)
+    a.search()
+
+    res = len(a.path)
+
+    assert res == 5
+
+def test_AS_3():
+    finput = "PathFinder-test3.txt"
+    origin, destinations, graph = parse_input(finput)
+
+    a = AS(origin, destinations, "A*", finput, graph)
+    a.search()
+
+    res = len(a.path)
+
+    assert res == 3
