277945e4d0d8359c07a2736c19ae1712e2b180bc
Keeth <87603596+Keeth-Christy@users.noreply.github.com>
2026-04-19 13:01:07 +1000

    renamed tests
    renamed tests based off alecs branch, unsure what the graph changes are so didn't wanna do a PR lol

---
+++ tests/test_graphs.py
@@ -0,0 +1,77 @@
+from algorithms import BFS, Greed, DFS, Beam
+from algorithms import parse_input
+
+def test_BFS():
+    finput = "PathFinder-test.txt"
+    origin, destinations, graph = parse_input(finput)
+
+    a = BFS(origin, destinations, "BFS", finput, graph)
+    a.search()
+    res = len(a.path)
+
+    assert res == 4
+def test_Greed_1():
+    finput = "PathFinder-test.txt"
+    origin, destinations, graph = parse_input(finput)
+
+    a = Greed(origin, destinations, "Greed", finput, graph)
+    a.search()
+
+    res = len(a.path)
+
+    assert res == 3
+
+def test_Greed_1():
+    finput = "PathFinder-test2.txt"
+    origin, destinations, graph = parse_input(finput)
+
+    a = Greed(origin, destinations, "Greed", finput, graph)
+    a.search()
+
+    res = len(a.path)
+
+    assert res == 5
+
+def test_DFS_1():
+    finput = "PathFinder-test.txt"
+    origin, destinations, graph = parse_input(finput)
+
+    a = DFS(origin, destinations, "DFS", finput, graph)
+    a.search()
+
+    res = len(a.path)
+
+    assert res == 4
+
+def test_DFS_2():
+    finput = "PathFinder-test2.txt"
+    origin, destinations, graph = parse_input(finput)
+
+    a = DFS(origin, destinations, "DFS", finput, graph)
+    a.search()
+
+    res = len(a.path)
+
+    assert res == 5
+
+def test_Beam_1():
+    finput = "PathFinder-test.txt"
+    origin, destinations, graph = parse_input(finput)
+
+    a = Beam(origin, destinations, "Beam Search", finput, graph)
+    a.search()
+
+    res = len(a.path)
+
+    assert res == 3
+
+def test_Beam_2():
+    finput = "PathFinder-test2.txt"
+    origin, destinations, graph = parse_input(finput)
+
+    a = Beam(origin, destinations, "Beam Search", finput, graph)
+    a.search()
+
+    res = len(a.path)
+
+    assert res == 5
